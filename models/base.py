ec32b7a07b4c84db73f0bf01b9d002554a990a84
Patrick Deubel <patrick.deubel@gmail.com>
2021-05-26 15:42:10 +0200

    base.py: Reconstruction and sampled images now logged to TensorBoard, ROC curve is now calculated at the end of the training and also logged to TensorBoard.

--- models/base.py
+++ models/base.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pytorch_lightning as pl
 import torch
-import torchvision.utils as vutils
+from sklearn.metrics import roc_curve, auc
 from torch import optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -80,50 +81,68 @@ class BaseVAE(pl.LightningModule):
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
         tensorboard_logs = {'avg_val_loss': avg_loss}
 
-        if self.current_epoch % 5 == 0 or self.current_epoch == (self.trainer.max_epochs - 1):
+        if self.current_epoch % 5 == 0:
             self.sample_images()
+
+        if self.current_epoch == (self.trainer.max_epochs - 1):
+            # We are in the last epoch, calculate ROC and AUC and log to tensorboard
+            self.sample_images()
+            self.calculate_roc_auc()
 
         return {'val_loss': avg_loss, 'log': tensorboard_logs}
 
-    def sample_images(self, save=True, display=False):
+    def calculate_pixelwise_differences(self, dataloader):
+        all_labels = []
+        all_pixelwise_differences = []
+
+        for batch, labels in dataloader:
+            predictions = self.generate(batch)
+
+            pixelwise_difference = torch.mean(torch.abs(predictions - batch), dim=(1, 2, 3))
+
+            all_labels = np.concatenate([all_labels, labels])
+            all_pixelwise_differences = np.concatenate([all_pixelwise_differences, pixelwise_difference], axis=0)
+
+        return all_labels, all_pixelwise_differences
+
+    def calculate_roc_auc(self):
+        all_labels, all_pixelwise_differences = self.calculate_pixelwise_differences(self.sample_dataloader)
+
+        val_dataloader_abnormal, _ = self.get_dataloader(train_split=False, abnormal_data=True, shuffle=True)
+        _labels, _pixelwise_differences = self.calculate_pixelwise_differences(val_dataloader_abnormal)
+
+        all_labels = np.concatenate([all_labels, _labels])
+        all_pixelwise_differences = np.concatenate([all_pixelwise_differences, _pixelwise_differences])
+
+        fpr, tpr, thresholds = roc_curve(all_labels, all_pixelwise_differences)
+        roc_auc = auc(fpr, tpr)
+
+        best_threshold = thresholds[np.argmax(tpr - fpr)]
+
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve (area = {:.2f})'.format(roc_auc))
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+
+        self.logger.experiment.add_figure("ROC Curve", plt.gcf(), global_step=self.current_epoch)
+
+    def sample_images(self):
         # Get sample reconstruction image
         test_input, test_label = next(iter(self.sample_dataloader))
         test_input = test_input.to(self.curr_device)
         test_label = test_label.to(self.curr_device)
         recons = self.generate(test_input, labels=test_label)
 
-        if save:
-            vutils.save_image(recons.data,
-                              f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
-                              f"recons_{self.logger.name}_{self.current_epoch}.png",
-                              normalize=True,
-                              nrow=12)
-
-        if display:
-            plt.imshow(vutils.make_grid(recons.data, normalize=True, nrow=12).permute(2, 1, 0).numpy())
-            plt.title("Reconstructed images")
-            plt.show()
-
-        # vutils.save_image(test_input.data,
-        #                   f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
-        #                   f"real_img_{self.logger.name}_{self.current_epoch}.png",
-        #                   normalize=True,
-        #                   nrow=12)
+        self.logger.experiment.add_images("reconstructions", recons, global_step=self.current_epoch)
 
         try:
-            samples = self.sample(144,
-                                  self.curr_device,
-                                  labels=test_label)
-            if save:
-                vutils.save_image(samples.cpu().data,
-                                  f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
-                                  f"{self.logger.name}_{self.current_epoch}.png",
-                                  normalize=True,
-                                  nrow=12)
-            if display:
-                plt.imshow(vutils.make_grid(samples.data, normalize=True, nrow=12).permute(2, 1, 0).numpy())
-                plt.title("Sampled images")
-                plt.show()
+            samples = self.sample(144, self.curr_device, labels=test_label)
+            self.logger.experiment.add_images("samples", samples, self.current_epoch)
         except:
             pass
 
@@ -166,64 +185,50 @@ class BaseVAE(pl.LightningModule):
             return optims
 
     def train_dataloader(self):
-        transform = self.data_transforms()
+        dataloader, self.num_train_imgs = self.get_dataloader(train_split=True, abnormal_data=False, shuffle=True)
 
-        if self.params['dataset'] == 'celeba':
-            dataset = CelebA(root=self.params['data_path'],
-                             split="train",
-                             transform=transform,
-                             download=False)
-        elif self.params['dataset'] == "concrete-cracks":
-            dataset = ConcreteCracksDataset(root_dir=self.params['data_path'],
-                                            split="train",
-                                            abnormal_data=False,
-                                            transform=transform)
-        elif self.params['dataset'] == "SDNET2018":
-            dataset = SDNet2018(root_dir=self.params['data_path'],
-                                split="train",
-                                abnormal_data=False,
-                                transform=transform)
-        else:
-            raise ValueError('Undefined dataset type')
-
-        self.num_train_imgs = len(dataset)
-
-        return DataLoader(dataset,
-                          batch_size=self.params['batch_size'],
-                          shuffle=True,
-                          drop_last=True,
-                          **self.additional_dataloader_args)
+        return dataloader
 
     def val_dataloader(self):
+        self.sample_dataloader, _ = self.get_dataloader(train_split=False, abnormal_data=False, shuffle=True)
+        self.num_val_imgs = len(self.sample_dataloader)
+
+        return self.sample_dataloader
+
+    def get_dataloader(self, train_split: bool, abnormal_data: bool = False, shuffle: bool = True):
         transform = self.data_transforms()
 
+        split = "train" if train_split else "val"
+
         if self.params['dataset'] == 'celeba':
+            if not train_split:
+                # CelebA uses no validation split, this comes from original author, but CelebA is not used in this
+                # project anyway
+                split = "test"
             dataset = CelebA(root=self.params['data_path'],
-                             split="test",
+                             split=split,
                              transform=transform,
                              download=False)
         elif self.params['dataset'] == 'concrete-cracks':
             dataset = ConcreteCracksDataset(root_dir=self.params['data_path'],
-                                            split="val",
-                                            abnormal_data=False,
+                                            split=split,
+                                            abnormal_data=abnormal_data,
                                             transform=transform)
         elif self.params['dataset'] == 'SDNET2018':
             dataset = SDNet2018(root_dir=self.params['data_path'],
-                                split="val",
-                                abnormal_data=False,
+                                split=split,
+                                abnormal_data=abnormal_data,
                                 transform=transform)
         else:
             raise ValueError('Undefined dataset type')
 
-        self.sample_dataloader = DataLoader(dataset,
-                                            batch_size=self.params['batch_size'],
-                                            shuffle=True,
-                                            drop_last=True,
-                                            **self.additional_dataloader_args)
+        dataloader = DataLoader(dataset,
+                                batch_size=self.params['batch_size'],
+                                shuffle=shuffle,
+                                drop_last=True,
+                                **self.additional_dataloader_args)
 
-        self.num_val_imgs = len(self.sample_dataloader)
-
-        return self.sample_dataloader
+        return dataloader, len(dataset)
 
     def data_transforms(self):
 
