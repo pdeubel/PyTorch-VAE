7117f55dc77824170ed5ade6b752f3c9fe6e8092
Patrick Deubel <patrick.deubel@gmail.com>
2021-06-01 14:58:42 +0200

    adVAEMNIST: Trying to reproduce results from the Tensorflow repository

--- models/adVAE_MNIST.py
+++ models/adVAE_MNIST.py
@@ -38,6 +38,17 @@ from torch.nn import functional as F
 from models import BaseVAE
 
 
+def kl_divergence(mu, log_var):
+    return 0.5 * torch.sum(-log_var - 1 + log_var.exp() + torch.square(mu), dim=1)
+
+
+def split_z(z, latent_dim):
+    z_mu = z[:, :latent_dim]
+    z_log_var = z[:, latent_dim:]
+
+    return z_mu, z_log_var
+
+
 class Encoder(pl.LightningModule):
 
     def __init__(self, in_channels: int, latent_dim: int):
@@ -49,27 +60,47 @@ class Encoder(pl.LightningModule):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=(3, 3), stride=1,
                                padding=1)
 
-        self.activation = nn.ELU()
+        nn.init.kaiming_normal_(self.conv1.weight)
+
+        self.activation = nn.LeakyReLU()
 
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1,
                                padding=1)
+
+        nn.init.kaiming_normal_(self.conv2.weight)
 
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+        nn.init.kaiming_normal_(self.conv3.weight)
+
         self.conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1,
                                padding=1)
+
+
+        nn.init.kaiming_normal_(self.conv4.weight)
 
         self.conv5 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+
+        nn.init.kaiming_normal_(self.conv5.weight)
+
         self.conv6 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+
+        nn.init.kaiming_normal_(self.conv6.weight)
+
         self.fc1 = nn.Linear(in_features=7 * 7 * 64, out_features=512)
+
+        nn.init.kaiming_normal_(self.fc1.weight)
+
         self.fc2 = nn.Linear(in_features=512, out_features=latent_dim * 2)
+
+        nn.init.kaiming_normal_(self.fc2.weight)
 
     def forward(self, x: torch.Tensor) -> List[torch.Tensor]:
         """
@@ -94,10 +125,9 @@ class Encoder(pl.LightningModule):
         x = self.activation(self.fc1(x))
         z_params = self.fc2(x)
 
-        z_mu = z_params[:, :self.latent_dim]
-        z_log_var = z_params[:, self.latent_dim:]
+        z_mu, z_sigma = split_z(z_params, self.latent_dim)
 
-        return [z_mu, z_log_var]
+        return [z_mu, z_sigma]
 
 
 class Decoder(pl.LightningModule):
@@ -108,9 +138,13 @@ class Decoder(pl.LightningModule):
         self.latent_dim = latent_dim
 
         self.fc1 = nn.Linear(in_features=self.latent_dim, out_features=512)
-        self.activation = nn.ELU()
+
+        nn.init.kaiming_normal_(self.fc1.weight)
+
+        self.activation = nn.LeakyReLU()
         # out_features is the output shape of the last conv layer in the encoder
         self.fc2 = nn.Linear(in_features=512, out_features=(64 * 7 * 7))
+        nn.init.kaiming_normal_(self.fc2.weight)
 
         # They seem to use
         self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1,
@@ -118,20 +152,32 @@ class Decoder(pl.LightningModule):
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+        nn.init.kaiming_normal_(self.conv1.weight)
+        nn.init.kaiming_normal_(self.conv2.weight)
+
         self.conv_transpose1 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=(3, 3), stride=2,
                                                   padding=1, output_padding=1)
+
+        nn.init.kaiming_normal_(self.conv_transpose1.weight)
 
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+        nn.init.kaiming_normal_(self.conv3.weight)
+
         self.conv_transpose2 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=(3, 3), stride=2,
                                                   padding=1, output_padding=1)
+        nn.init.kaiming_normal_(self.conv_transpose2.weight)
 
         self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1,
                                padding=1)
 
+        nn.init.kaiming_normal_(self.conv4.weight)
+
         self.conv5 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(3, 3), stride=1,
                                padding=1)
+
+        nn.init.kaiming_normal_(self.conv5.weight)
 
         self.output_activation = nn.Sigmoid()
 
@@ -168,15 +214,21 @@ class Transformer(pl.LightningModule):
         self.latent_dim = latent_dim
 
         self.fc1 = nn.Linear(in_features=self.latent_dim, out_features=2 * self.latent_dim)
-        self.activation = nn.ELU()
+        nn.init.kaiming_normal_(self.fc1.weight)
+
+        self.old_weights = torch.clone(self.fc1.weight).detach().numpy()
+        self.current_weight = None
+
+        self.activation = nn.LeakyReLU()
 
     def forward(self, z: torch.Tensor) -> List[torch.Tensor]:
+        self.current_weight = self.fc1.weight.detach().numpy()
+        self.old_weights = torch.clone(self.fc1.weight).detach().numpy()
         z_params = self.activation(self.fc1(z))
 
-        z_T_mu = z_params[:, :self.latent_dim]
-        z_T_log_var = z_params[:, self.latent_dim:]
+        z_T_mu, z_T_sigma = split_z(z_params, self.latent_dim)
 
-        return [z_T_mu, z_T_log_var]
+        return z_T_mu, z_T_sigma
 
 
 # noinspection PyPep8Naming
@@ -203,7 +255,11 @@ class adVAEMNIST(BaseVAE):
 
         self.save_hyperparameters()
 
-    def reparameterize(self, mu: torch.Tensor, log_var: torch.Tensor) -> torch.Tensor:
+        torch.autograd.set_detect_anomaly(True)
+
+        self.synthesized_anomalies = None
+
+    def reparameterize(self, mu: torch.Tensor, sigma: torch.Tensor) -> torch.Tensor:
         """
         Reparameterization trick to sample from N(mu, var) from
         N(0,1).
@@ -211,15 +267,15 @@ class adVAEMNIST(BaseVAE):
         :param log_var: (Tensor) Standard deviation of the latent Gaussian [B x D]
         :return: (Tensor) [B x D]
         """
-        std = torch.exp(0.5 * log_var)
-        eps = torch.randn_like(std)
-        return eps * std + mu
 
-    def encode(self, x: torch.Tensor) -> List[torch.Tensor]:
-        mu, log_var = self.encoder(x)
-        z = self.reparameterize(mu, log_var)
+        eps = torch.randn_like(sigma)
+        return eps * sigma + mu
 
-        return [mu, log_var, z]
+    def encode(self, x: torch.Tensor):
+        z_mu, z_sigma = self.encoder(x)
+        z = self.reparameterize(z_mu, z_sigma)
+
+        return z_mu, z_sigma, z
 
     def forward(self, x: torch.Tensor, optimizer_idx=0) -> dict:
         # TODO maybe refine this so some computations don't have to be executed twice per batch
@@ -228,28 +284,63 @@ class adVAEMNIST(BaseVAE):
         # self.last_x = self.encoder.state_dict()["encoder.0.0.weight"].clone()
 
         # Training Step 1
-        mu, log_var, z = self.encode(x)
+        if optimizer_idx == 0:
+            with torch.no_grad():
+                z_mu, z_sigma, z = self.encode(x)
 
-        mu_T, log_var_T = self.transformer(z)
-        z_T = self.reparameterize(mu_T, log_var_T)
+            z_mu_T, z_sigma_T = self.transformer(z)
+            z_T = self.reparameterize(z_mu_T, z_sigma_T)
 
-        x_r = self.decoder(z)
-        x_T_r = self.decoder(z_T)
+            x_r = self.decoder(z)
+            x_T_r = self.decoder(z_T)
 
-        mu_r, log_var_r, _ = self.encode(x_r)
-        mu_T_r, log_var_T_r, _ = self.encode(x_T_r)
+            self.synthesized_anomalies = x_T_r
+
+            with torch.no_grad():
+                mu_r, sigma_r, _ = self.encode(x_r)
+                mu_T_r, sigma_T_r, _ = self.encode(x_T_r)
+
+                test_mu_T_r = torch.mean(torch.sum(mu_T_r, dim=1), dim=0)
+                test_sigma_T_r = torch.mean(torch.sum(sigma_T_r, dim=1), dim=0)
+
+            print("hello")
+        elif optimizer_idx == 1:
+
+            z_mu, z_sigma, z = self.encode(x)
+
+            with torch.no_grad():
+                z_mu_T, z_sigma_T = self.transformer(z)
+
+            z_T = self.reparameterize(z_mu_T, z_sigma_T)
+
+            with torch.no_grad():
+                x_r = self.decoder(z)
+                x_T_r = self.decoder(z_T)
+
+                self.synthesized_anomalies = x_T_r
+
+            mu_r, sigma_r, _ = self.encode(x_r)
+            mu_T_r, sigma_T_r, _ = self.encode(x_T_r)
+
+            test_mu_T_r = torch.mean(torch.sum(mu_T_r, dim=1), dim=0)
+            test_sigma_T_r = torch.mean(torch.sum(sigma_T_r, dim=1), dim=0)
+
+            print("Hello")
+
+        else:
+            raise RuntimeError("adVAE only supports exactly two optimizers")
 
         return {"x": x,
                 "x_r": x_r,
                 "x_T_r": x_T_r,
-                "mu": mu,
-                "log_var": log_var,
-                "mu_T": mu_T,
-                "log_var_T": log_var_T,
+                "z_mu": z_mu,
+                "z_sigma": z_sigma,
+                "z_mu_T": z_mu_T,
+                "z_sigma_T": z_sigma_T,
                 "mu_r": mu_r,
-                "log_var_r": log_var_r,
+                "sigma_r": sigma_r,
                 "mu_T_r": mu_T_r,
-                "log_var_T_r": log_var_T_r}
+                "sigma_T_r": sigma_T_r}
 
         # # Training Step 2
         # # self.decoder.freeze()
@@ -287,79 +378,79 @@ class adVAEMNIST(BaseVAE):
         """
         optimizer_idx = kwargs['optimizer_idx']
 
+        x = results["x"]
+        x_r = results["x_r"]
+        x_T_r = results["x_T_r"]
+        z_mu = results["z_mu"]
+        z_sigma = results["z_sigma"]
+        z_mu_T = results["z_mu_T"]
+        z_sigma_T = results["z_sigma_T"]
+        mu_r = results["mu_r"]
+        sigma_r = results["sigma_r"]
+        mu_T_r = results["mu_T_r"]
+        sigma_T_r = results["sigma_T_r"]
+
         if optimizer_idx == 0:
             # Update Generator and Transformer
-            x = results["x"]
-            x_r = results["x_r"]
-            x_T_r = results["x_T_r"]
-            mu = results["mu"]
-            log_var = results["log_var"]
-            mu_T = results["mu_T"]
-            log_var_T = results["log_var_T"]
-            mu_r = results["mu_r"]
-            log_var_r = results["log_var_r"]
-            mu_T_r = results["mu_T_r"]
-            log_var_T_r = results["log_var_T_r"]
+
+            L_T_term_1 = 0.5 * z_sigma_T - 0.5 * z_sigma
+            L_T_term_2 = (z_sigma.exp() + torch.square(z_mu - z_mu_T)) / (2 * z_sigma_T.exp())
+            L_T_term_3 = -0.5
+
+            L_T = torch.sum(L_T_term_1 + L_T_term_2 + L_T_term_3, dim=1)
 
             L_G_z_term_1 = F.mse_loss(x, x_r)
-            L_G_z_term_2 = torch.mean(-0.5 * torch.sum(1 + log_var_r - mu_r ** 2 - log_var_r.exp(), dim=1), dim=0)
+            L_G_z_term_2 = kl_divergence(mu_r, sigma_r)
             L_G_z = L_G_z_term_1 + L_G_z_term_2
 
             L_G_z_T_term_1 = self.params["m_x"] - F.mse_loss(x_r, x_T_r)
             L_G_z_T_term_1 = torch.maximum(torch.zeros_like(L_G_z_T_term_1), L_G_z_T_term_1)
 
-            L_G_z_T_term_2 = self.params["m_z"] - torch.mean(
-                -0.5 * torch.sum(1 + log_var_T_r - mu_T_r ** 2 - log_var_T_r.exp(), dim=1), dim=0)
+            L_G_z_T_term_2 = self.params["m_z"] - kl_divergence(mu_T_r, sigma_T_r)
             L_G_z_T_term_2 = torch.maximum(torch.zeros_like(L_G_z_T_term_2), L_G_z_T_term_2)
 
             L_G_z_T = L_G_z_T_term_1 + L_G_z_T_term_2
 
             L_G = L_G_z + L_G_z_T
 
-            L_T_term_1 = 0.5 * log_var_T - 0.5 * log_var
-            L_T_term_2 = (log_var.exp() + torch.square(mu - mu_T)) / (2 * log_var_T.exp())
-            L_T_term_3 = -0.5
-
-            L_T = torch.sum(L_T_term_1 + L_T_term_2 + L_T_term_3, dim=1)
-
             loss = torch.mean((self.lambda_T * L_T) + (self.lambda_G * L_G), dim=0)
+
+            loss_T = torch.mean(self.lambda_T * L_T, dim=0)
+            loss_G = torch.mean(self.lambda_G * L_G, dim=0)
 
             # This is used when loss_function is called with optimizer_idx=1, then the encoder loss is added and
             # the summed loss of generator+transformer and encoder is logged
             self.summary_loss = loss
 
-            return {"loss": loss, "loss_G_and_T": loss, "loss_G": L_G, "loss_G_z": L_G_z, "loss_G_z_T": L_G_z_T,
-                    "loss_T": torch.mean(L_T, dim=0),
-                    "l_G_z_term_1": L_G_z_term_1, "l_G_z_term_2": L_G_z_term_2,
-                    "l_G_z_T_term_1": L_G_z_T_term_1, "l_G_z_T_term_2": L_G_z_T_term_2,
-                    "l_T_term_1": torch.mean(torch.sum(L_T_term_1, dim=1), dim=0),
-                    "l_T_term_2": torch.mean(torch.sum(L_T_term_2, dim=1), dim=0),
-                    "mu": torch.mean(mu), "var": torch.mean(log_var.exp()),
-                    "mu_T": torch.mean(mu_T), "var_T": torch.mean(log_var_T.exp())}
+            # return {"loss": loss, "loss_G_and_T": loss, "loss_G": L_G, "loss_G_z": L_G_z, "loss_G_z_T": L_G_z_T,
+            #         "loss_T": torch.mean(L_T, dim=0),
+            #         "l_G_z_term_1": L_G_z_term_1, "l_G_z_term_2": L_G_z_term_2,
+            #         "l_G_z_T_term_1": L_G_z_T_term_1, "l_G_z_T_term_2": L_G_z_T_term_2,
+            #         "l_T_term_1": torch.mean(torch.sum(L_T_term_1, dim=1), dim=0),
+            #         "l_T_term_2": torch.mean(torch.sum(L_T_term_2, dim=1), dim=0),
+            #         "mu": torch.mean(mu), "var": torch.mean(log_var.exp()),
+            #         "mu_T": torch.mean(mu_T), "var_T": torch.mean(log_var_T.exp())}
+            return {"loss": loss,
+                    "L_G": loss_G, "L_T": loss_T}
         elif optimizer_idx == 1:
             # Update Encoder
-            x = results["x"]
-            x_r = results["x_r"]
-            mu = results["mu"]
-            log_var = results["log_var"]
-            mu_r = results["mu_r"]
-            log_var_r = results["log_var_r"]
-            mu_T_r = results["mu_T_r"]
-            log_var_T_r = results["log_var_T_r"]
-
             L_E_term_1 = F.mse_loss(x, x_r)
-            L_E_term_2 = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0)
-            L_E_term_3 = self.params["m_z"] - torch.mean(-0.5 * torch.sum(1 + log_var_r - mu_r ** 2 - log_var_r.exp(), dim=1), dim=0)
+            L_E_term_2 = kl_divergence(z_mu, z_sigma)
+
+            L_E_term_3 = self.params["m_z"] - kl_divergence(mu_r, sigma_r)
             L_E_term_3 = torch.maximum(torch.zeros_like(L_E_term_3), L_E_term_3)
-            L_E_term_4 = self.params["m_z"] - torch.mean(-0.5 * torch.sum(1 + log_var_T_r - mu_T_r ** 2 - log_var_T_r.exp(), dim=1), dim=0)
+
+            L_E_term_4 = self.params["m_z"] - kl_divergence(mu_T_r, sigma_T_r)
             L_E_term_4 = torch.maximum(torch.zeros_like(L_E_term_4), L_E_term_4)
+
             L_E = L_E_term_1 + L_E_term_2 + L_E_term_3 + L_E_term_4
 
             loss = torch.mean(L_E, dim=0)
 
-            return {"loss": loss, "loss_summary": self.summary_loss + loss, "loss_E": L_E,
-                    "l_E_term_1": L_E_term_1, "l_E_term_2": L_E_term_2, "l_E_term_3": L_E_term_3,
-                    "l_E_term_4": L_E_term_4}
+            # return {"loss": loss, "loss_summary": self.summary_loss + loss, "loss_E": L_E,
+            #         "l_E_term_1": L_E_term_1, "l_E_term_2": L_E_term_2, "l_E_term_3": L_E_term_3,
+            #         "l_E_term_4": L_E_term_4}
+            return {"loss": loss, "loss_summary": self.summary_loss + loss}
 
     def training_step(self, batch, batch_idx, optimizer_idx=0):
         real_img, labels = batch
@@ -392,6 +483,9 @@ class adVAEMNIST(BaseVAE):
 
         if self.current_epoch % 1 == 0 or self.current_epoch == (self.trainer.max_epochs - 1):
             self.sample_images()
+            self.logger.experiment.add_images("Synthesized anomalies",
+                                              self.synthesized_anomalies,  # Use make_grid to normalize
+                                              global_step=self.current_epoch)  #
 
         self.logger.experiment.log({'avg_val_loss': avg_loss})
 
